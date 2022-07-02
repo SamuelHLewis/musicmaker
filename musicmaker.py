056629fb7b7c3dd62f41b457870b2ee96c9a8598
SamuelHLewis <sam_h_l@icloud.com>
2022-07-02 16:35:12 +0100

    Add trial step for midi writing

--- musicmaker.py
+++ musicmaker.py
@@ -1,5 +1,5 @@
 import argparse
-from utils.midi_utils import read_midi
+from utils.midi_utils import read_midi, write_midi
 
 parser = argparse.ArgumentParser(description="Generate new music")
 parser.add_argument('--midi', type=str, help="The name of the midi file to use as input")
@@ -10,3 +10,9 @@ if __name__=="__main__":
     midi_contents = read_midi(args.midi)
     print(len(midi_contents['notes']))
     print(len(midi_contents['durations']))
+
+    midi_contents = {
+        'notes': ['C2', 'D2', 'E2', 'F2', 'G2', 'A2', 'B2', 'C3'],
+        'durations': [1,1,1,1,1,1,1,1]
+    }
+    write_midi(midi_contents, 'trial.mid')
